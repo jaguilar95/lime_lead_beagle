38326df356578d2c34c6746410e29b407540f224
Leonardo <josue.aguilar1995@gmail.com>
2022-06-26 13:06:57 -0500

    fixed small association bug

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -15,10 +15,14 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
-Product.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
